427c4104bb0962d7f937295221d98ab2026f7f17
Likhina_Uladzislau <lih.well@mail.ru>
2021-09-22 20:08:01 +0300

    hw3 likhina

---
+++ src/main/java/edu/academy/jc/ikhina/hw3/Phone.java
@@ -0,0 +1,67 @@
+package edu.academy.jc.ikhina.hw3;
+
+public class Phone {
+
+    private int Number;
+    private String Model;
+    private double Weight;
+
+    public void receiveCall(String name){
+        System.out.println("Is calling "+name);
+    }
+
+    public int getNumber() {
+        return Number;
+    }
+
+    public Phone(int number, String model, double weight) {
+        this(number,model);
+        this.Weight = weight;
+    }
+
+    public void setNumber(int number) {
+        Number = number;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setModel(String model) {
+        Model = model;
+    }
+
+    public double getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(double weight) {
+        Weight = weight;
+    }
+
+    public Phone(int number, String model) {
+        this.Number = number;
+        this.Model = model;
+    }
+
+    public Phone (){
+       this.Number = 0;
+       this.Model = null;
+       this.Weight=0;
+
+    }
+
+    public void receiveCall(String name,int number){
+        System.out.println("Is calling "+name+", number - "+number);
+    }
+    /* public int sendMessage(int i,int...)
+     {
+      return i;
+
+     }*/
+}
+
+
+
+
+
