53bd9c76f88d4a7c73f270e0e3db0a9806bc9593
TsimafeiTESTgit <90564062+TsimafeiTESTgit@users.noreply.github.com>
2021-09-22 17:57:18 +0300

    Commit HW2 and HW3(start)

--- src/main/java/edu/academy/jc/TsimafeiTESTgit/HW3/Clock.java
+++ src/main/java/edu/academy/jc/TsimafeiTESTgit/HW3/Clock.java
@@ -1,4 +1,15 @@
 package edu.academy.jc.TsimafeiTESTgit.HW3;
 
 public class Clock {
+int alarmHours, alarmMinutes;
+
+//public String setAlarm(int hours, int minutes){
+//return
+//}
+
+
+
+    public static void main(String[] args) {
+
+    }
 }
