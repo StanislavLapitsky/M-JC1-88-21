c70cf217fcc11b708f47e5e7fbe3181334760122
“Artem <3947710@mail.ru>
2021-09-25 15:43:52 +0300

    My Homework №5 Metlushko

---
+++ src/main/java/edu/academy/jc/metlushko/ht4/StrOp.java
@@ -0,0 +1,68 @@
+package edu.academy.jc.metlushko.ht4;
+
+
+public class StrOp {
+    public static void main(String[] args) {
+        StrOpTest.checkRemoveNumbers("23qwe324ersdf435  345 ret fdg ");
+        StrOpTest.checkrepeatChars("1aas2qwe3ghj4","1aassqweeeghjjjj");
+        StrOpTest.checkrepeatCharsHard("1ab3zxc4","1abababzxcxcxcxc");
+
+
+    }
+
+    public static String removeNumbers(String s){
+        char[] chars=s.toCharArray();
+        StringBuilder str= new StringBuilder();
+        for (char aChar : chars) {
+            if (!Character.isDigit(aChar)) {
+                str.append(aChar);
+            }
+        }
+        return str.toString();
+    }
+
+    public static String repeatChars(String s){
+        char[] chars=s.toCharArray();
+        StringBuilder stringBuilder= new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if(Character.isDigit(chars[i])&&i!=0){
+                stringBuilder.append(checkCharIsDigit(chars[i-1],chars[i]));
+            }
+            else {
+                stringBuilder.append(chars[i]);
+            }
+        }
+        return stringBuilder.toString();
+    }
+    public static String checkCharIsDigit(char ch,char n){
+        StringBuilder s= new StringBuilder();
+        for (int k=n-'0'; (k-1)>0; k--) {
+            s.append(ch);
+        }
+
+        return s.toString();
+    }
+
+    public static String repeatCharsHard(String s){
+        char[] chars=s.toCharArray();
+        StringBuilder stringBuilder= new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if(Character.isDigit(chars[i])&&i!=0&&i!=1){
+                stringBuilder.append(checkCharIsDigitHard(chars[i-2],chars[i-1],chars[i]));
+            }
+            else {
+                stringBuilder.append(chars[i]);
+            }
+        }
+        return stringBuilder.toString();
+    }
+    public static String checkCharIsDigitHard(char ch1,char ch2,char n){
+        StringBuilder s= new StringBuilder();
+        for (int k=n-'0'; (k-1)>0; k--) {
+            s.append(ch1).append(ch2);
+        }
+
+        return s.toString();
+    }
+
+}
---
+++ src/main/java/edu/academy/jc/metlushko/ht4/StrOpTest.java
@@ -0,0 +1,33 @@
+package edu.academy.jc.metlushko.ht4;
+
+
+public class StrOpTest {
+    public static void checkRemoveNumbers(String s){
+        String str=s.replaceAll("[0-9]","");
+        if(str.equals(StrOp.removeNumbers(s))){
+            System.out.println(StrOp.removeNumbers(s));
+            System.out.println("All right in method removeNumbers");
+        }
+        else {
+            System.out.println("Not right working! ");
+        }
+    }
+    public static void checkrepeatChars(String s,String result){
+        if((StrOp.repeatChars(s)).equals(result)){
+            System.out.println("All right in method repeatChars ");
+        }
+        else {
+            System.out.println("Not right working! ");
+        }
+
+    }
+    public static void checkrepeatCharsHard(String s,String result){
+        if((StrOp.repeatCharsHard(s)).equals(result)){
+            System.out.println("All right in method repeatCharsHard ");
+        }
+        else {
+            System.out.println("Not right working! ");
+        }
+
+    }
+}
