c91111c0ac107be3e0279bf039ca938746754ff1
AlexanderYarokhovich <yarokhovichalex@mail.ru>
2021-09-26 20:42:57 +0300

    HW3 deleted comments

--- src/main/java/edu/academy/jc/yarokhovich/hw3/Clock.java
+++ src/main/java/edu/academy/jc/yarokhovich/hw3/Clock.java
@@ -8,10 +8,10 @@ public class Clock {
         if (((hours >= 0) && (hours < 24)) && ((minutes >= 0) && (minutes < 60))) {
             alarmHours=hours;
             alarmMinutes=minutes;
-            return "Good";//считаю что задача понаписанию метода не совсем корректна, нужно его делать не стренговым а
-        }else { return null;}//бул, что бы вернул тру или фолс, тогда его можно проще использовать.
-    }                        //еще неудобно что нельзя отсюда вернуть значения, хотел массив или List вернуть, но тип
-                             // метода стринг.
+            return "Good";
+        }else { return null;}
+    }
+
     public String checkAlarm(int hours, int minutes) {
         if (hours == alarmHours && minutes == alarmMinutes) {
             return  "Chime";
@@ -23,7 +23,7 @@ public class Clock {
     public int checkTimeCount(int hours, int minutes) {
         int temp;
         String check = setAlarm(hours,minutes);
-        if(check != null && !check.trim().isEmpty()) {//проверка на не пустую строку!
+        if(check != null && !check.trim().isEmpty()) {//check for a non-empty string!
             if (minutes == 0) {
                 if (hours <= 12) {
                     temp = hours;
@@ -33,7 +33,7 @@ public class Clock {
             else {
                 temp = 0;
             }
-        }else {System.out.print("Alarm! "); return 0;}//если строка пустая значит значения не корректны выводит Alarm и 0
+        }else {System.out.print("Alarm! "); return 0;}//if the string is empty, then the values are not correct, Alarm and 0 are displayed
         return temp;
     }
 }
