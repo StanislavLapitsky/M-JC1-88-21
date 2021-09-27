fd5f47b9065f8bd1b7d719730e2f807841c851b5
“Artem <3947710@mail.ru>
2021-09-27 13:40:10 +0300

    new solution

--- src/main/java/edu/academy/jc/metlushko/lesson4/Star.java
+++ src/main/java/edu/academy/jc/metlushko/lesson4/Star.java
@@ -1,39 +1,37 @@
 package edu.academy.jc.metlushko.lesson4;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 
 public class Star {
 
     public static void main(String[] args) {
-        int []arr=initializationRandomIntArray(20);
-        System.out.println(Arrays.toString(maxNumberInArray(arr,5)));
+        int []arr=initializationRandomIntArray(29);
+        System.out.println(Arrays.toString(maxNumberInArray(arr,8)));
     }
 
     public static int[] initializationRandomIntArray (int number){
         Random random=new Random();
         int [] arr=new int[number];
         for (int i = 0; i < arr.length; i++) {
-            arr[i]=random.nextInt(25);
+            arr[i]=random.nextInt(28);
         }
         return arr;
     }
 
     public static int[] maxNumberInArray(int [] arr,int n){
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
-
-        for (int i = 0; i < arr.length-1; i++) {
-            if(arr[i]==arr[i+1]){
-                arr[i+1]=0;
-            }
-        }
+        int j=0;
         Arrays.sort(arr);
         int[]arraysInt=new int[n];
-        for (int j = arr.length-1, i=0; j >((arr.length-1)-n);i++, j--) {
-            arraysInt[i]=arr[j];
+        System.out.println(Arrays.toString(arr));
+        for (int i = arr.length-1; i > 0; i--) {
+            while (j<n){
+                if(arr[i]!=arr[i-1]){
+                    arraysInt[j]=arr[i];
+                    j++;
+                }
+                break;
+            }
         }
         return arraysInt;
     }
 }
-
