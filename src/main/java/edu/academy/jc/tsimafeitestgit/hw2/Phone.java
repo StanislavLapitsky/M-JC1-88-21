5665b11acb129b553cfd21979a31354dfff9d278
TsimafeiTESTgit <90564062+TsimafeiTESTgit@users.noreply.github.com>
2021-09-27 17:31:01 +0300

    separate main class from Phone

--- src/main/java/edu/academy/jc/tsimafeitestgit/hw2/Phone.java
+++ src/main/java/edu/academy/jc/tsimafeitestgit/hw2/Phone.java
@@ -1,26 +1,24 @@
 package edu.academy.jc.tsimafeitestgit.hw2;
 
 public class Phone {
-    String number;
+    int number;
     String model;
     int weight;
     String name = "name";
 
-    public Phone(String newNumber, String newModel, int newWeight) {
+    public Phone(int newNumber, String newModel, int newWeight) {
         number = newNumber;
         model = newModel;
         weight = newWeight;
     }
 
-
-    public Phone(String newNumber, String newModel) {
+    public Phone(int newNumber, String newModel) {
         this.number = newNumber;
         this.model = newModel;
     }
 
     public Phone() {
     }
-
 
     public String receiveCall() {
         System.out.println("Звонит " + "{" + name + "}");
@@ -31,25 +29,10 @@ public class Phone {
         return;
     }
 
-    public String getNumber() {
+    public int getNumber() {
         return this.number;
     }
 
-    public static void main(String[] args) {
-        Phone ph1 = new Phone("355-85-85", "samsung", 14);
-        Phone ph2 = new Phone("355-42-42", "apple", 1);
-        Phone ph3 = new Phone("355-13-13", "zion", 11);
-
-        System.out.println("phone 1: " + "Model is: " + ph1.model + " number is: " + ph1.number + " weight is: " + ph1.weight);
-        System.out.println("phone 2: " + "Model is: " + ph2.model + " number is: " + ph2.number + " weight is: " + ph2.weight);
-        System.out.println("phone 3: " + "Model is: " + ph3.model + " number is: " + ph3.number + " weight is: " + ph3.weight);
-
-        System.out.println("number is: " + ph1.getNumber());
-        System.out.println("number is: " + ph2.getNumber());
-        System.out.println("number is: " + ph3.getNumber());
-
-        Phone ph4 = new Phone("375-29-233", "assmung", 8);
-        System.out.println(ph1.receiveCall());
-    }
+    //вынесем main в отдельный класс, а тут оставим только "конструктор"...
 }
 
