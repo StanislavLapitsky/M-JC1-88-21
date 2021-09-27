b81581eb2fc8dba0a3a98ea15b76ba6ca89e6e14
Vladimir-Bon <78255397+Vladimir-Bon@users.noreply.github.com>
2021-09-27 20:05:54 +0300

    Loops homework 4

--- src/main/java/edu/academy/jc/bondarenko/hw4-5/src/Loops.java
+++ src/main/java/edu/academy/jc/bondarenko/hw4-5/src/Loops.java
@@ -3,7 +3,6 @@ import java.util.Random;
 
 public class Loops {
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
         Loops l = new Loops();
         // task 1
