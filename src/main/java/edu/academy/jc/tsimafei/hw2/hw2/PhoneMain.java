40e20de9445bc900c0cb01637a61d18a6893fb1f
TsimafeiTESTgit <90564062+TsimafeiTESTgit@users.noreply.github.com>
2021-09-29 15:44:11 +0300

    commies!

---
+++ src/main/java/edu/academy/jc/tsimafei/hw2/hw2/PhoneMain.java
@@ -0,0 +1,22 @@
+package edu.academy.jc.tsimafei.hw2.hw2;
+
+public class PhoneMain {
+    public static void main(String[] args) {
+        Phone ph1 = new Phone(3558585, "samsung", 14);
+        Phone ph2 = new Phone(3554242, "apple", 1);
+        Phone ph3 = new Phone(3551313, "zion", 11);
+
+        System.out.println("phone 1: " + "Model is: " + ph1.model + " number is: " + ph1.number + " weight is: " + ph1.weight);
+        System.out.println("phone 2: " + "Model is: " + ph2.model + " number is: " + ph2.number + " weight is: " + ph2.weight);
+        System.out.println("phone 3: " + "Model is: " + ph3.model + " number is: " + ph3.number + " weight is: " + ph3.weight);
+
+        System.out.println("number is: " + ph1.getNumber());
+        System.out.println("number is: " + ph2.getNumber());
+        System.out.println("number is: " + ph3.getNumber());
+
+        Phone ph4 = new Phone(37529233, "Szael", 8);
+        System.out.println(ph1.receiveCall());
+    }
+}
+
+//почему-то выводит 'name' отдельно...
