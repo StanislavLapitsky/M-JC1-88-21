624789149fc856aff1266bdb968cc2742cf99cf3
TsimafeiTESTgit <90564062+TsimafeiTESTgit@users.noreply.github.com>
2021-09-29 15:50:08 +0300

    branch hw3 commit'n'push?

---
+++ src/main/java/edu/academy/jc/tsimafei/hw3/Clock.java
@@ -0,0 +1,16 @@
+package edu.academy.jc.tsimafei.hw3;
+
+public class Clock {
+int alarmHours, alarmMinutes;
+
+//public String setAlarm(int hours, int minutes){
+//return
+//}
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
+//added to separate branch??
