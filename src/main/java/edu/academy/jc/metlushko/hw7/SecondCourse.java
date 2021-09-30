ce9ae65603c89363831a8ecaf8375733b9df4e6c
“Artem <3947710@mail.ru>
2021-09-30 21:49:32 +0300

    Metlushko HW7 Foods

---
+++ src/main/java/edu/academy/jc/metlushko/hw7/SecondCourse.java
@@ -0,0 +1,22 @@
+package edu.academy.jc.metlushko.hw7;
+
+public class SecondCourse extends Food{
+    private String name;
+    private FoodType foodType;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+    @Override
+    public void setFoodType(FoodType foodType){
+        this.foodType=foodType;
+    }
+    @Override
+    public FoodType getFoodType() {
+        return this.foodType;
+    }
+
+}
