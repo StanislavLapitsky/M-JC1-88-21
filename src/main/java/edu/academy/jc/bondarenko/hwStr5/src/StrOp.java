092dd43c4fba0c494f17ed067ca71ad4b99c67a5
Vladimir-Bon <78255397+Vladimir-Bon@users.noreply.github.com>
2021-10-01 17:14:05 +0300

    hw #5

---
+++ src/main/java/edu/academy/jc/bondarenko/hwStr5/src/StrOp.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class StrOp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        StrOp str = new StrOp();
+        System.out.println("Please, enter the string ");
+        String s = in.nextLine();
+        str.removeNumbers(s);
+        System.out.println();
+    }
+
+    public void removeNumbers(String s) {
+        System.out.println(s.replaceAll("[0-9]", ""));
+    }
+
+    public void repeatChars(String s) {
+    }
+}
