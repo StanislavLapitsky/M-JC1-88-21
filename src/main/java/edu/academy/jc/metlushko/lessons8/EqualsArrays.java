bb4e0a90063672959bc286219cc5f27742adba35
“Artem <3947710@mail.ru>
2021-10-07 21:35:56 +0300

    lessons8 getTwoEqualsArrays

---
+++ src/main/java/edu/academy/jc/metlushko/lessons8/EqualsArrays.java
@@ -0,0 +1,84 @@
+package edu.academy.jc.metlushko.lessons8;
+import java.util.*;
+
+public class EqualsArrays {
+    public static void main(String[] args) {
+        int [] arrays=generationRandomArray(new int[10],10);
+        int [] arraysA = new int[arrays.length / 2];
+        int [] arraysB = new int[arrays.length / 2];
+
+        Arrays.sort(arrays);
+        System.out.println(Arrays.toString(arrays));
+        System.out.println("Sum arrays: "+sumArray(arrays)+"\n");
+        getEqualsArrays(arrays, arraysA, arraysB);
+        System.out.println(Arrays.toString(arraysA));
+        System.out.println("Sum arrays A: "+sumArray(arraysA)+"\n");
+        System.out.println(Arrays.toString(arraysB));
+        System.out.println("Sum arrays B: "+sumArray(arraysB));
+    }
+
+    public static int[] generationRandomArray(int[] n, int max) {
+        Random random = new Random();
+        for (int i = 0; i < n.length; i++) {
+            n[i] = random.nextInt(max);
+        }
+        return n;
+    }
+
+    public static int sumArray(int[] n) {
+        int sum = 0;
+        for (int j : n) {
+            sum += j;
+        }
+        return sum;
+    }
+    public static int getDifference(int a,int b){
+        if((a-b)%2==1){
+            return (a-b-1)/2;
+        }
+        if ((a-b)%2==0){
+            return (a-b)/2;
+        }
+        return 0;
+
+    }
+
+    public static void getEqualsArrays(int [] n,int[] arraysA, int[] arraysB){
+        System.arraycopy(n,0,arraysA,0,arraysA.length);
+        System.arraycopy(n,5,arraysB,0,arraysA.length);
+        for (int b = arraysB.length-1; b >0; b--) {
+            int a = 0;
+            while ( a < arraysA.length) {
+                if(sumArray(arraysB)==sumArray(arraysA)){
+                    return;
+                }
+                int dif=getDifference(sumArray(arraysB),sumArray(arraysA));
+                if((arraysB[b]-arraysA[a])<=dif&&(arraysB[b]-arraysA[a])>0){
+                    int p=arraysA[a];
+                    arraysA[a]=arraysB[b];
+                    arraysB[b]=p;
+                    break;
+                }
+                a++;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
