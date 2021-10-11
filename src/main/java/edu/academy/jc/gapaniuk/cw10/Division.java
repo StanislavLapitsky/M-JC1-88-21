1a5816dd3ff7362fb193da0c52e9082149f5c254
GapSerg <gapserg@tut.by>
2021-10-11 20:58:59 +0300

    CW 10 corr

--- src/main/java/edu/academy/jc/gapaniuk/cw10/Division.java
+++ src/main/java/edu/academy/jc/gapaniuk/cw10/Division.java
@@ -7,26 +7,7 @@ public class Division {
     public Integer divide(List arguments) throws NullPointerException, IndexOutOfBoundsException, ClassCastException, ArithmeticException {
 
         return ((Integer) arguments.get(0) )/ ((Integer)arguments.get(1));
-//       try {
-//
-//           return ((Integer) arguments.get(0) )/ ((Integer)arguments.get(1));
-//     }
-//        catch (NullPointerException e){
-//            System.out.println("error NullPointerException");
-//          return null;
-//        }
-//       catch (IndexOutOfBoundsException e){
-//           System.out.println("error IndexOutOfBoundsException");
-//           return null;
-//       }
-//       catch (ClassCastException e){
-//           System.out.println("error ClassCastException");
-//           return null;
-//       }
-//       catch (ArithmeticException e){
-//           System.out.println("error ArithmeticException");
-//           return null;
-//       }
+
 
 
     }
