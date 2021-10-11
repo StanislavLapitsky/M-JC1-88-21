57e849952505dfae8692b7e2617cfea0244f1dc1
GapSerg <gapserg@tut.by>
2021-10-11 20:48:12 +0300

    CW 10

---
+++ src/main/java/edu/academy/jc/gapaniuk/cw10/Division.java
@@ -0,0 +1,33 @@
+package edu.academy.jc.gapaniuk.cw10;
+
+
+import java.util.List;
+
+public class Division {
+    public Integer divide(List arguments) throws NullPointerException, IndexOutOfBoundsException, ClassCastException, ArithmeticException {
+
+        return ((Integer) arguments.get(0) )/ ((Integer)arguments.get(1));
+//       try {
+//
+//           return ((Integer) arguments.get(0) )/ ((Integer)arguments.get(1));
+//     }
+//        catch (NullPointerException e){
+//            System.out.println("error NullPointerException");
+//          return null;
+//        }
+//       catch (IndexOutOfBoundsException e){
+//           System.out.println("error IndexOutOfBoundsException");
+//           return null;
+//       }
+//       catch (ClassCastException e){
+//           System.out.println("error ClassCastException");
+//           return null;
+//       }
+//       catch (ArithmeticException e){
+//           System.out.println("error ArithmeticException");
+//           return null;
+//       }
+
+
+    }
+}
---
+++ src/main/java/edu/academy/jc/gapaniuk/cw10/TooLongNameException.java
@@ -0,0 +1,7 @@
+package edu.academy.jc.gapaniuk.cw10;
+
+public class TooLongNameException extends Exception{
+    public TooLongNameException(String message) {
+        super(message);
+    }
+}
