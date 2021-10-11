2a2a474ac6a9ba4401227bec034bef1dae5524ac
“Artem <3947710@mail.ru>
2021-10-11 10:32:03 +0300

    lesson9 metlushko

--- src/main/java/edu/academy/jc/metlushko/lesson9/ComparatorSortAge.java
+++ src/main/java/edu/academy/jc/metlushko/lesson9/ComparatorSortAge.java
@@ -1,10 +1,12 @@
 package edu.academy.jc.metlushko.lesson9;
 
 import java.util.Comparator;
+import java.util.Objects;
 
 public class ComparatorSortAge implements Comparator<User> {
     @Override
     public int compare(User obj1,User obj2){
-        return obj1.getName().compareTo(obj2.getName());
+
+        return Objects.requireNonNull(obj1).getAge().compareTo(Objects.requireNonNull(obj2).getAge());
     }
 }
--- src/main/java/edu/academy/jc/metlushko/lesson9/ComparatorSortName.java
+++ src/main/java/edu/academy/jc/metlushko/lesson9/ComparatorSortName.java
@@ -1,10 +1,12 @@
 package edu.academy.jc.metlushko.lesson9;
 
 import java.util.Comparator;
+import java.util.Objects;
 
 public class ComparatorSortName implements Comparator<User> {
     @Override
-    public int compare(User obj1,User obj2){
-        return obj1.getName().compareTo(obj2.getName());
+    public int compare(User obj1, User obj2){
+
+        return Objects.requireNonNull(obj1).getName().compareTo(Objects.requireNonNull(obj2).getName());
     }
 }
