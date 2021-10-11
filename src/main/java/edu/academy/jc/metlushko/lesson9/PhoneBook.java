1b5cd82dd6db9ff4ac7f6ac0917c1ab50cde1577
“Artem <3947710@mail.ru>
2021-10-11 09:44:39 +0300

    lesson9 metlushko

--- src/main/java/edu/academy/jc/metlushko/lesson9/PhoneBook.java
+++ src/main/java/edu/academy/jc/metlushko/lesson9/PhoneBook.java
@@ -4,41 +4,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PhoneBook {
-    private Map<String,Integer> hashMap;
+    private Map<String, Integer> hashMap;
 
     public PhoneBook() {
-        hashMap=new HashMap<>();
+        hashMap = new HashMap<>();
     }
 
-    public void addPhone(String name,Integer number){
-        hashMap.put(name,number);
+    public void addPhone(String name, Integer number) {
+        hashMap.put(name, number);
     }
-    public Integer getPhoneByName(String name){
-       return hashMap.get(name);
+
+    public Integer getPhoneByName(String name) {
+        return hashMap.get(name);
     }
 
     @Override
     public String toString() {
-        return "hashMap=" + hashMap ;
+        return "hashMap=" + hashMap;
     }
 
     public static void main(String[] args) {
-        PhoneBook phoneBook=new PhoneBook();
-        phoneBook.addPhone("Artem",3947719);
-        phoneBook.addPhone("Kolya",7845123);
-        phoneBook.addPhone("Vacia",9528465);
-        phoneBook.addPhone("Vacia",9528465);
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.addPhone("Artem", 3947719);
+        phoneBook.addPhone("Kolya", 7845123);
+        phoneBook.addPhone("Vacia", 9528465);
+        phoneBook.addPhone("Vacia", 9528465);
 
         System.out.println(phoneBook.getPhoneByName("Artem"));
         System.out.println(phoneBook.getPhoneByName("Oly"));
         System.out.println(phoneBook);
-
-
-
-
-
-
-
 
 
     }
