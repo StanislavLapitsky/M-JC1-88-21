f6cdee66b0ea396fd61726a8d6453eda9637d77a
Likhina_Uladzislau <lih.well@mail.ru>
2021-10-24 17:43:06 +0300

    Please help2

--- src/main/java/edu/academy/jc/likhina/hw10and11/MyPhoneBook.java
+++ src/main/java/edu/academy/jc/likhina/hw10and11/MyPhoneBook.java
@@ -1,20 +1,19 @@
 package edu.academy.jc.likhina.hw10and11;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.Map.Entry;
 
 public class MyPhoneBook implements PhoneBook {
-    private static HashMap<String, Integer> phoneBookMap = new HashMap<>();
+    private static final HashMap<String, Integer> phoneBookMap = new HashMap<>();
+
+
 
     @Override
     public void addUser(String name, int number) {
         if (name == null) {
             System.out.println("Name can't be NULL");
-        } else if (name == "") {
+        } else if (name.length() == 0) {
             System.out.println("Name can't be empty");
         } else if (phoneBookMap.containsKey(name)) {
             System.out.println("A contact with this name already exists");
@@ -30,53 +29,103 @@ public class MyPhoneBook implements PhoneBook {
             System.out.println("There is no contact with that name");
             return 0;
         } else
-            System.out.println(name+" == "+phoneBookMap.get(name));
-        return 1;
-        //is it necessary to get values from the phonebook text file here?
+            return phoneBookMap.get(name);
     }
 
     @Override
     public String getNameByNumber(int number) {
-        if(!phoneBookMap.containsValue(number)){
-            System.out.println("There is no contact with this number!");
+        String key = "";
+        for (Entry<String, Integer> entry : phoneBookMap.entrySet()) {
+            if (entry.getValue() == number) {
+                key = entry.getKey();
+                break;
+            }
         }
-            return null;
-        //please help me how to get the key value?
+        return key;
     }
 
     @Override
     public void storeToFile(File phoneBookFile) {
         try {
-            FileWriter writer = new FileWriter(phoneBookFile);
-            BufferedWriter bufferedWriter = new BufferedWriter(writer);
-            for (Map.Entry entry : phoneBookMap.entrySet()) {
-                bufferedWriter.write(entry.getKey() + ";" + entry.getValue() + ";" + "\n");
+            try (FileWriter writer = new FileWriter(phoneBookFile)) {
+                try (BufferedWriter bufferedWriter = new BufferedWriter(writer)) {
+                    for (Entry entry : phoneBookMap.entrySet())
+                        bufferedWriter.write(entry.getKey() + ";" + entry.getValue() + ";" + "\n");
+                }
+
             }
-            bufferedWriter.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
+
 
     @Override
     public void loadFromFile(File phoneBookFile) {
-//please help, I don't understand on what principle and how it should work?
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(phoneBookFile));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (phoneBookFile.exists()) {
+            try {
+                String line = null;
+                if (reader != null) {
+                    line = reader.readLine();
+                }
+                String[] arrStr = new String[0];
+                if (line != null) {
+                    arrStr = line.split(";");
+                }
+                if (line != null && !line.equals("")) {
+                    phoneBookMap.put(arrStr[0], Integer.valueOf(arrStr[1]));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(phoneBookMap);
+            }
+        }
     }
 
-    public static void main(String[] args) {
-        File phoneBookFile = new File("D:/phoneBookFile.txt");
-        PhoneBook phoneBook = new MyPhoneBook();
-        phoneBook.addUser("Andrey", 293593817);
-        phoneBook.addUser("Sveta", 253697814);
-        phoneBook.addUser("Sasha", 447896541);
-        phoneBook.addUser(null, 45654855);
-        phoneBook.addUser("Sasha", 954887444);
-        phoneBook.addUser("Karl", 123456789);
-        phoneBook.addUser("", 1445646565);
 
-        phoneBook.storeToFile(phoneBookFile);
-        phoneBook.getNumberByName("Sveta");
-        phoneBook.getNumberByName("Sasha");
-        phoneBook.getNumberByName("ksdcmskdm");
+    public static void main(String[] args) {
+
+        File phoneBookFile = new File("D:/PhoneBook.txt");
+        //I don't understand how to create a new book with a new address(I understand that you need to change the address in the variable),
+        // and it stopped writing to the file, shows that it is empty, but reads from it(busy file?)
+
+        PhoneBook phoneBook1 = new MyPhoneBook();
+        PhoneBook phoneBook2 = new MyPhoneBook();
+
+
+        phoneBook1.storeToFile(phoneBookFile);
+        phoneBook2.storeToFile(phoneBookFile);
+
+
+        phoneBook1.addUser("Andrey", 293593817);
+        phoneBook1.addUser("Sveta", 253697814);
+        phoneBook1.addUser("Sasha", 447896541);
+        phoneBook1.addUser(null, 45654855);
+        phoneBook1.addUser("Sasha", 954887444);
+        phoneBook1.addUser("Karl", 123456789);
+        phoneBook1.addUser("", 1445646565);
+
+        phoneBook2.addUser("Nikita", 4544);
+        phoneBook1.loadFromFile(phoneBookFile);
+        phoneBook2.loadFromFile(phoneBookFile);
+
+        System.out.println(phoneBook1.getNumberByName("Sveta") == 253697814);
+        System.out.println(phoneBook1.getNumberByName("Sasha") == 954887444);
+        System.out.println(phoneBook1.getNumberByName("ksdcmskdm") == 1445646565);
+
+
+        System.out.println(phoneBook1.getNameByNumber(293593817).equals("Andrey"));
+        System.out.println(phoneBook1.getNameByNumber(29359381).equals("Andrey"));
+
     }
 }
