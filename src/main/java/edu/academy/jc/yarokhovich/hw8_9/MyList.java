7bcb54a308263988e899f43dd560c1db9a63bdcb
AlexanderYarokhovich <yarokhovichalex@mail.ru>
2021-11-01 13:19:33 +0300

    HW8 and 9

--- src/main/java/edu/academy/jc/yarokhovich/hw8_9/MyList.java
+++ src/main/java/edu/academy/jc/yarokhovich/hw8_9/MyList.java
@@ -3,18 +3,18 @@ package edu.academy.jc.yarokhovich.hw8_9;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MyList<Integer> {//list
-    private int maxElements;
-    private List<Integer> list = new ArrayList<>();
+public class MyList extends ArrayList {
+    private final int maxElements;
+    private final List<Integer> list;
 
     public MyList(int maxElements) {
         this.maxElements = maxElements;
+        this.list = new ArrayList<>();
     }
 
     public boolean addElem(Integer n) {
         if (list.size() < maxElements) {
-            list.add(n);
-            return true;
+            return  list.add(n);
         }
         return false;
     }
